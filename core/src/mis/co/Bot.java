4d4c7d06779fa600f6da22ee96edeb6201751677
yo mismo <yo@mismo>
2023-01-23 19:17:20 +0100

    .

--- core/src/mis/co/Bot.java
+++ core/src/mis/co/Bot.java
@@ -3,9 +3,17 @@ package mis.co;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.Random;
+
 public class Bot {
-    int posX, posY;
     Texture textura;
+    Random random = new Random();
+
+    float posX, posY;
+    float velX = 2;
+    float accX = 0.05f;
+    int width = 20;
+    int height = 20;
 
     public Bot(int posX, int posY, Texture textura) {
         this.posX = posX;
@@ -15,13 +23,16 @@ public class Bot {
 
 
     void mover(){
-        posX++;
-        posY++;
+        if (random.nextInt(200) == 0) {
+            velX = velX * -1;
+        }
 
+        posX = posX + velX;
+        velX = velX + accX;
 
     }
 
     void render(SpriteBatch batch){
-        batch.draw(textura, posX, posY);
+        batch.draw(textura, posX, posY, width, height);
     }
 }
